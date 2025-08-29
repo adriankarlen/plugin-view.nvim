c5c7aaf53863fd8998369673cae8d5b9196534c5
Adrian Karlen <adrian.karlen@spp.se>
2025-08-29 15:26:27 +0200

    feat: add update bindings

--- lua/plugin-view/bindings.lua
+++ lua/plugin-view/bindings.lua
@@ -1,4 +1,4 @@
-local utils = require("plugin-view.utils")
+local utils = require "plugin-view.utils"
 
 ---@private
 local M = {}
@@ -9,11 +9,21 @@ M.setup = function(buf, win)
   utils.add_keymap(buf, "n", "<ESC>", function()
     vim.api.nvim_win_close(win, true)
   end)
-  utils.add_keymap(buf, "n", "r", function()
-    vim.api.nvim_win_close(win, true)
-    require("plugin-view").open()
+  utils.add_keymap(buf, "n", "U", function()
+    vim.pack.update()
+  end)
+  utils.add_keymap(buf, "n", "u", function()
+    -- Get current line and extract plugin name until first space
+    local current_line = vim.api.nvim_get_current_line()
+    local plugin_name = current_line:match "^([^%s]+)"
+
+    if not plugin_name then
+      vim.notify("Plugin not found in current line", vim.log.levels.ERROR)
+      return
+    end
+
+    vim.pack.update { plugin_name }
   end)
 end
-
 
 return M
