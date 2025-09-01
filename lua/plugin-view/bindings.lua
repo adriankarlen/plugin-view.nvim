0ef63093a30f9892feea93450943e0c44083e95e
Adrian Karlen <adrian.karlen@spp.se>
2025-09-01 09:30:00 +0200

    feat: add delete bind

--- lua/plugin-view/bindings.lua
+++ lua/plugin-view/bindings.lua
@@ -2,6 +2,11 @@ local utils = require "plugin-view.utils"
 
 ---@private
 local M = {}
+
+local get_plugin_name = function(line)
+  return line:match "^([^%s]+)"
+end
+
 M.setup = function(buf, win)
   utils.add_keymap(buf, "n", "q", function()
     vim.api.nvim_win_close(win, true)
@@ -13,16 +18,21 @@ M.setup = function(buf, win)
     vim.pack.update()
   end)
   utils.add_keymap(buf, "n", "u", function()
-    -- Get current line and extract plugin name until first space
-    local current_line = vim.api.nvim_get_current_line()
-    local plugin_name = current_line:match "^([^%s]+)"
-
+    local plugin_name = get_plugin_name(vim.api.nvim_get_current_line())
     if not plugin_name then
       vim.notify("Plugin not found in current line", vim.log.levels.ERROR)
       return
     end
 
     vim.pack.update { plugin_name }
+  end)
+  utils.add_keymap(buf, "n", "d", function()
+    local plugin_name = get_plugin_name(vim.api.nvim_get_current_line())
+    if not plugin_name then
+      vim.notify("Plugin not found in current line", vim.log.levels.ERROR)
+      return
+    end
+    vim.pack.delete { plugin_name }
   end)
 end
 
